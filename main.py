e16dd9effaaa85217f82df917b8de816b233fe9a
KrishnaSai96-bit <112385885+KrishnaSai96-bit@users.noreply.github.com>
2023-11-18 17:59:39 +0530

    Changes

--- main.py
+++ main.py
@@ -14,17 +14,17 @@ from sqlalchemy.sql import text
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine) #creates tables in MySQL database based on the definitions in model.py
 
-class RecipeBase(BaseModel):
+class RecipeBO(BaseModel):
     idRECIPE: int
     RECIPE_Title: str
-    RECIPE_Category: int
-    RECIPE_CookingTime: int
+    RECIPE_Category: str
+    RECIPE_CookingTime: str
     
      
-class RecipeDetailsBase(BaseModel):
+class RecipeDetailsBO(BaseModel):
     Ingredients: str
-    Recipe_Setps: str
-    idRECIPE: str
+    Recipe_Steps: str
+    idRECIPE: int
 
 
 def get_db():
@@ -36,13 +36,34 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)] #db and class
 
-@app.post("/posts/", status_code=status.HTTP_201_CREATED)
-async def create_post(post: RecipeBase, db:db_dependency):
-    # db_post = models.Post(**post.model_dump())
+@app.post("/recipe/", status_code=status.HTTP_201_CREATED)
+async def create_recipe(post: RecipeBO, db:db_dependency):
     post = post.model_dump()
-    # db.add(db_post)
-    # db.commit()
-    statement = text("INSERT INTO posts(idRECIPE, RECIPE_Title, RECIPE_Category, RECIPE_CookingTime) VALUES(:idRECIPE, :RECIPE_Title, :RECIPE_Category, :RECIPE_CookingTime)")
-    #by writing a sql stmt (native sql)
+    statement = text("INSERT INTO recipe(idRECIPE, RECIPE_Title, RECIPE_Category, RECIPE_CookingTime) VALUES(:idRECIPE, :RECIPE_Title, :RECIPE_Category, :RECIPE_CookingTime)")
     db.execute(statement, post)
     db.commit()
+    
+@app.post("/recipe_details/", status_code=status.HTTP_201_CREATED)
+async def create_recipe_detials(post: RecipeDetailsBO, db:db_dependency):
+    post = post.model_dump()
+    statement = text("INSERT INTO recipe_details(Ingredients, Recipe_Steps, idRECIPE) VALUES(:Ingredients, :Recipe_Steps, :idRECIPE)")
+    db.execute(statement, post)
+    db.commit()
+    
+@app.get("/cookbook/{recipe_id}", status_code=status.HTTP_200_OK) #get menthod
+async def get_recipe(recipe_id:int, db:db_dependency):
+    statement = text("SELECT * FROM recipe WHERE idRECIPE = :recipe_id")
+    result = db.execute(statement, {'recipe_id': recipe_id})
+    recipe = result.fetchall()
+    recipe_dict = {'idRECIPE':[0][0], 'RECIPE_Title':recipe[0][1], 'RECIPE_Category':recipe[0][2], 'RECIPE_CookingTime':recipe[0][3]}
+    return recipe_dict
+
+@app.get("/cookbook_details/", status_code=status.HTTP_200_OK) 
+async def get_recipe_details(db:db_dependency):
+    statement = text("select * from recipe, recipe_details where recipe.idRECIPE = recipe_details.idRECIPE")
+    result = db.execute(statement)
+    recipe = result.fetchall()
+    recipe_dict = { 'idRECIPE':[0][0], 'RECIPE_Title':recipe[0][1], 'RECIPE_Category':recipe[0][2], 
+                    'RECIPE_CookingTime':recipe[0][3], 'Ingredients':recipe[0][4], 
+                    'Recipe_Steps':recipe[0][5], 'idRECIPE':recipe[0][6]}
+    return recipe_dict
--- models.py
+++ models.py
@@ -10,14 +10,14 @@ class Recipe(Base):
     idRECIPE = Column(Integer, primary_key=True, index=True) #first column in db, want to be able to index (faster performance)
     RECIPE_Title = Column(String(50), unique=True) #instantiate string with varchar 50
     RECIPE_Category = Column(String(50), unique=True)
-    RECIPE_CookingTime = Column(Integer, unique=True)
+    RECIPE_CookingTime = Column(String(50), unique=True)
 
 class Recipe_Details(Base):
     __tablename__ = 'recipe_details'
 
     id = Column(Integer, primary_key=True, index=True)
     Ingredients = Column(String(1000))
-    Recipe_Setps = Column(String(1000))
+    Recipe_Steps = Column(String(1000))
     idRECIPE = Column(Integer) #foreign key
  
-#Updated on 11th
+#Updated on 18th
