c757a48026d47362145029db94ad13d0753c794c
KrishnaSai96-bit <112385885+KrishnaSai96-bit@users.noreply.github.com>
2023-11-29 13:29:32 +0530

    29th

--- main.py
+++ main.py
@@ -9,10 +9,19 @@ from sqlalchemy.orm import Session
 
 from sqlalchemy.sql import text
 
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import PlainTextResponse
+from starlette.exceptions import HTTPException as StarletteHTTPException
+
+
 
 # instantiate app
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine) #creates tables in MySQL database based on the definitions in model.py
+
+@app.exception_handler(StarletteHTTPException)
+async def http_exception_handler(request, exc):
+    return PlainTextResponse(str(exc.detail), status_code=exc.status_code)
 
 class RecipeBO(BaseModel):
     idRECIPE: int
@@ -36,34 +45,72 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)] #db and class
 
-@app.post("/recipe/", status_code=status.HTTP_201_CREATED)
-async def create_recipe(post: RecipeBO, db:db_dependency):
-    post = post.model_dump()
+@app.post("/CookBook/CreateRecipe", status_code=status.HTTP_201_CREATED)
+async def create_recipe(recipeBO: RecipeBO, db:db_dependency):
+    recipeBO = recipeBO.model_dump()
     statement = text("INSERT INTO recipe(idRECIPE, RECIPE_Title, RECIPE_Category, RECIPE_CookingTime) VALUES(:idRECIPE, :RECIPE_Title, :RECIPE_Category, :RECIPE_CookingTime)")
-    db.execute(statement, post)
+    db.execute(statement, recipeBO)
     db.commit()
     
-@app.post("/recipe_details/", status_code=status.HTTP_201_CREATED)
-async def create_recipe_detials(post: RecipeDetailsBO, db:db_dependency):
-    post = post.model_dump()
+@app.post("/CookBook/CreateReciepDetails", status_code=status.HTTP_201_CREATED)
+async def create_recipe_detials(recipeDetailsBO: RecipeDetailsBO, db:db_dependency):
+    recipeDetailsBO = recipeDetailsBO.model_dump()
     statement = text("INSERT INTO recipe_details(Ingredients, Recipe_Steps, idRECIPE) VALUES(:Ingredients, :Recipe_Steps, :idRECIPE)")
-    db.execute(statement, post)
+    db.execute(statement, recipeDetailsBO)
     db.commit()
     
-@app.get("/cookbook/{recipe_id}", status_code=status.HTTP_200_OK) #get menthod
+@app.get("/CookBook/GetRecipe{recipe_id}", status_code=status.HTTP_200_OK)
 async def get_recipe(recipe_id:int, db:db_dependency):
     statement = text("SELECT * FROM recipe WHERE idRECIPE = :recipe_id")
     result = db.execute(statement, {'recipe_id': recipe_id})
     recipe = result.fetchall()
-    recipe_dict = {'idRECIPE':[0][0], 'RECIPE_Title':recipe[0][1], 'RECIPE_Category':recipe[0][2], 'RECIPE_CookingTime':recipe[0][3]}
+    recipe_dict = {'idRECIPE':recipe[0][0], 'RECIPE_Title':recipe[0][1], 'RECIPE_Category':recipe[0][2], 'RECIPE_CookingTime':recipe[0][3]}
     return recipe_dict
 
-@app.get("/cookbook_details/", status_code=status.HTTP_200_OK) 
+#@app.get("/CookBook/GetRecipeDetails{recipe_id}", status_code=status.HTTP_200_OK) 
+@app.get("/CookBook/GetList", status_code=status.HTTP_200_OK)
 async def get_recipe_details(db:db_dependency):
-    statement = text("select * from recipe, recipe_details where recipe.idRECIPE = recipe_details.idRECIPE")
+    statement = text("SELECT recipe.idRECIPE, recipe.RECIPE_Title, recipe.RECIPE_Category, recipe.RECIPE_CookingTime, recipe_details.Ingredients, recipe_details.Recipe_Steps FROM recipe, recipe_details WHERE recipe.idRECIPE = recipe_details.idRECIPE")
     result = db.execute(statement)
+    recipes = result.fetchall()
+    # recipe_dict = { 'idRECIPE':recipe[0][0], 'RECIPE_Title':recipe[0][1], 'RECIPE_Category':recipe[0][2], 
+    #                 'RECIPE_CookingTime':recipe[0][3], 'Ingredients':recipe[0][4], 
+    #                 'Recipe_Steps':recipe[0][5], 'idRECIPE':recipe[0][6]}
+    # return recipe_dict
+    recipe_list = []
+    for i in range(len(recipes)):
+        recipe_dict = {'idRECIPE':recipes[i][0], 'RECIPE_Title':recipes[i][1], 'RECIPE_Category':recipes[i][2], 
+                     'RECIPE_CookingTime':recipes[i][3], 'Ingredients':recipes[i][4], 
+                     'Recipe_Steps':recipes[i][5]}
+        recipe_list.append(recipe_dict)
+    return recipe_list
+
+@app.get("/CookBook/GetListOfCategories{RECIPE_Category}", status_code=status.HTTP_200_OK)
+async def get_recipe_categories(RECIPE_Category: str, db:db_dependency):
+    statement = text("Select * from recipe where RECIPE_Category = :RECIPE_Category") 
+    result = db.execute(statement, {'RECIPE_Category': RECIPE_Category})
     recipe = result.fetchall()
-    recipe_dict = { 'idRECIPE':[0][0], 'RECIPE_Title':recipe[0][1], 'RECIPE_Category':recipe[0][2], 
-                    'RECIPE_CookingTime':recipe[0][3], 'Ingredients':recipe[0][4], 
-                    'Recipe_Steps':recipe[0][5], 'idRECIPE':recipe[0][6]}
-    return recipe_dict
+    if len(recipe) == 0:
+        raise HTTPException(status_code=404, detail='No Recipe is Found')
+    categories = []
+    for i in range(len(recipe)):
+        recipe_dict = {'idRECIPE':recipe[i][0], 'RECIPE_Title':recipe[i][1], 'RECIPE_CookingTime':recipe[i][3]}
+        categories.append(recipe_dict)
+    return categories
+
+    #  recipe_dict = {'idRECIPE':recipe[0][0], 'RECIPE_Title':recipe[0][1], 'RECIPE_Category':recipe[0][2], 'RECIPE_CookingTime':recipe[0][3]}
+    #  return recipe_dict
+     
+     
+     
+
+# @app.get("/posts/", status_code=status.HTTP_200_OK)
+# async def get_all_posts(db:db_dependency):
+#     statement = text("SELECT * FROM posts")
+#     result = db.execute(statement)
+#     post = result.fetchall()
+#     all_posts = []
+#     for i in range(len(post)):
+#         post_dict = {'id':post[i][0], 'title':post[i][1], 'content':post[i][2], 'user_id':post[i][3]}
+#         all_posts.append(post_dict)
+#     return all_posts
--- models.py
+++ models.py
@@ -8,9 +8,9 @@ class Recipe(Base):
     __tablename__ = 'recipe'
 
     idRECIPE = Column(Integer, primary_key=True, index=True) #first column in db, want to be able to index (faster performance)
-    RECIPE_Title = Column(String(50), unique=True) #instantiate string with varchar 50
-    RECIPE_Category = Column(String(50), unique=True)
-    RECIPE_CookingTime = Column(String(50), unique=True)
+    RECIPE_Title = Column(String(50)) #instantiate string with varchar 50
+    RECIPE_Category = Column(String(50))
+    RECIPE_CookingTime = Column(String(50))
 
 class Recipe_Details(Base):
     __tablename__ = 'recipe_details'
