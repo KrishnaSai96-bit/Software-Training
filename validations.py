eac4554099e99701fda1c5d5c4103d3d5b1850d6
KrishnaSai96-bit <112385885+KrishnaSai96-bit@users.noreply.github.com>
2023-12-11 18:58:58 +0530

    Validations P1

--- validations.py
+++ validations.py
@@ -16,8 +16,71 @@ def string_casefold(s):
 def string_find(s):
     return s.find("Monday")
 
-DataEntered = input("enter a string:")
+def string_center(s):
+    return s.center(22)
+
+def string_count(s):
+    return s.count("Monday")
+
+def string_encode(s):
+    return s.encode()
+
+def string_endswith(s):
+    return s.endswith(".")
+
+def string_expandtabs(s):
+    return s.expandtabs(2)
+
+def string_format(s):
+    return s.format(price = 49)
+
+# def string_format_map(s):
+#     return s.format_map(price = 49)
+
+def string_index(s):
+    return s.index("Starting")
+
+def string_isalnum(s):
+    return s.isalnum()
+
+def string_isalpha(s):
+    return s.isalpha()
+
+def string_isascii(s):
+    return s.isascii()
+
+def string_isdecimal(s):
+    return s.isdecimal()
+
+DataEntered = input("Enter a String:")
 #print (string_capitalize (DataEntered))
 #print (string_casefold (DataEntered))
-print (string_find (DataEntered))
+#print (string_find (DataEntered))
+#print (string_center (DataEntered))
+#print (string_count (DataEntered))
+#print (string_encode (DataEntered))
+#print (string_endswith (DataEntered))
+#print (string_expandtabs (DataEntered)) #Doubt
+#print (string_format (DataEntered)) #Doubt
+#print (string_format_map (DataEntered)) #Doubt
+#print (string_index ("Today is Monday and Starting of Every Week is Monday."))
+#print (string_isalnum (DataEntered))
+#print (string_isalpha (DataEntered))
+#print (string_isascii (DataEntered))
+print (string_isdecimal (DataEntered))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
